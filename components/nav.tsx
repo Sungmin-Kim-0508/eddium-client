1705cbe3dc2449fc0740f6d26045785fbfd9b1ad
Eddie Kim <sungmin.kim082@gmail.com>
2020-10-23 14:31:17 -0400

    logout and caching updated

--- components/nav.tsx
+++ components/nav.tsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react"
 import Link from 'next/link'
 import { Transition } from "@headlessui/react"
-import { MeQuery, useMeQuery } from "../generated/graphql"
+import { ApolloClient, useApolloClient } from '@apollo/client'
+import { useLogoutMutation, useMeQuery } from "../generated/graphql"
 import { toastNotification } from "../utils/toasters"
 
 const IconButton: React.FC = ({ children }) => (
@@ -18,15 +19,37 @@ const Anchor: React.FC<{ href: string }> = ({ children, href }) => (
   </Link>
 )
 
+const MenuButton: React.FC<{ isLoading: boolean, onClick?: React.MouseEventHandler<HTMLButtonElement> }> = ({ children, isLoading = true, onClick }) => (
+  <>
+    {isLoading && (
+      <button type="button" onClick={onClick} className="inline-flex items-center px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+        <svg className="animate-spin -ml-1 mr-3 h-5 w-5 text-accent-1" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+          <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+          <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        Processing
+      </button>
+    )}
+    {!isLoading && (
+      <button onClick={onClick} className="block px-4 py-2 text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out">
+        {children}
+      </button>
+    )}
+  </>
+)
+
 export default function Nav() {
   const [isOpen, setIsOpen] = useState(false)
   const openDropdown = () => {
     setIsOpen(!isOpen)
   }
 
-  const { data, error } = useMeQuery()
+  const apolloClient = useApolloClient();
+
+  const [logout, { loading: logoutLoading }] = useLogoutMutation()
+  const { data, error, loading: meLoading } = useMeQuery()
   
-  if (error) {
+  if (meLoading === false && error) {
     toastNotification.error(error?.message)
   }
 
@@ -96,7 +119,12 @@ export default function Nav() {
                           <Anchor href="/">New Story</Anchor>
                           <Anchor href="/register">Stories</Anchor>
                           <Anchor href="/register">Settings</Anchor>
-                          <Anchor href="/">Log out</Anchor>
+                          <MenuButton
+                            isLoading={logoutLoading}
+                            onClick={async () => {
+                            await logout()
+                            await apolloClient.resetStore()
+                          }}>Log out</MenuButton>
                         </div>
                     </div>
                     )}
